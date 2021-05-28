2875633771b940731d38639d129ec063f23afa73
Elvis Ruci <eruci18@epoka.edu.al>
2021-05-28 21:16:00 +0200

    Added Project Template

---
+++ Code/SDMS/src/main/java/Group2A/SDMS/SdmsApplication.java
@@ -0,0 +1,13 @@
+package Group2A.SDMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdmsApplication.class, args);
+	}
+
+}
---
+++ Code/SDMS/src/test/java/Group2A/SDMS/SdmsApplicationTests.java
@@ -0,0 +1,13 @@
+package Group2A.SDMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
