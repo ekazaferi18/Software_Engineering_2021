f80eaafc85948d6da61baf44fa9ff2818fe341bc
Elvis Ruci <eruci18@epoka.edu.al>
2021-06-16 09:18:30 +0200

    Update DatabaseScript.sql
    updated database

--- Code/Database Code/DatabaseScript.sql
+++ Code/Database Code/DatabaseScript.sql
@@ -5,13 +5,13 @@ use bms;
 create table Users(
 	userID int NOT NULL AUTO_INCREMENT,
     username varchar(10),
-    password varchar(20),
+    password varchar(60),
     firstname varchar(30),
     lastname varchar(30),
     email varchar(30),
     address varchar(50),
-    accesslevel varchar(10),
-    totalPoints int,
+    access_level varchar(10),
+    total_points int,
     wage double,
     primary key(userID),
     deleted boolean default false,
@@ -19,12 +19,12 @@ create table Users(
     updated_date timestamp default current_timestamp on update current_timestamp
 );
 
-create table ProductCategories(
+create table product_categories(
 	categoryID int NOT NULL AUTO_INCREMENT,
     name varchar(20),
     description varchar(50),
     primary key(categoryID),
-    deleted boolean  default false,
+    deleted boolean default false,
     created_date timestamp default current_timestamp,
     updated_date timestamp default current_timestamp on update current_timestamp
 );
@@ -36,9 +36,9 @@ create table Products(
     quantity int,
     price double,
     categoryID int,
-    expirationDate timestamp,
+    expiration_date timestamp,
     PRIMARY KEY(productID),
-    foreign key (categoryID) references ProductCategories(categoryID),
+    foreign key (categoryID) references product_categories(categoryID),
     deleted boolean  default false,
     created_date timestamp default current_timestamp,
     updated_date timestamp default current_timestamp on update current_timestamp
@@ -46,11 +46,11 @@ create table Products(
 
 create table Orders(
 	orderID int NOT NULL AUTO_INCREMENT,
-    paymentType varchar(10),
+    payment_type varchar(10),
     clientID int,
     sellerID int,
     total double,
-    pointsEarned int,
+    points_earned int,
     PRIMARY KEY(orderID),
     foreign key (clientID) references Users(userID),
     foreign key (sellerID) references Users(userID),
@@ -75,7 +75,7 @@ create table ProcurementOrderDetails(
     productID int,
     quantity int,
     pricePerUnit double,
-    totalPrice double,
+    total_price double,
     primary key(pOrderDetailID),
     foreign key (pOrderID) references ProcurementOrder(pOrderID),
     foreign key (productID) references Products(productID),
@@ -90,14 +90,14 @@ create table OrderDetails(
     orderID int,
     quantity int,
     pricePerUnit double,
-    TotalPrice double,
+    total_price double,
     PRIMARY KEY(orderDetailID),
     foreign key (productID) references Products(productID),
     foreign key (orderID) references Orders(orderID),
     deleted boolean  default false,
     created_date timestamp default current_timestamp,
     updated_date timestamp default current_timestamp on update current_timestamp
-);
+);	
 
 create table Deliveries(
 	deliveryID INT NOT NULL AUTO_INCREMENT,
@@ -170,7 +170,7 @@ CREATE TRIGGER priceUpdates before insert ON OrderDetails
  FOR EACH ROW 
 	BEGIN
     set new.pricePerUnit = (select price from products where productID = new.productID);
-    set new.totalPrice = new.pricePerUnit * new.quantity;
+    set new.total_price = new.pricePerUnit * new.quantity;
 	end$$
 delimiter ;
 
@@ -179,7 +179,7 @@ delimiter $$
 CREATE TRIGGER total after update ON OrderDetails for each row
     BEGIN
     declare sum double;
-    set sum = (select SUM(totalPrice) from OrderDetails where orderID = new.orderID);
+    set sum = (select SUM(total_price) from OrderDetails where orderID = new.orderID);
     update Orders set total = sum where orderID = new.orderID;
 	end$$
 delimiter ;
@@ -188,7 +188,7 @@ delimiter $$
 CREATE TRIGGER priceUpdatesPO before insert ON ProcurementOrderDetails
  FOR EACH ROW 
 	BEGIN
-    set new.totalPrice = new.pricePerUnit * new.quantity;
+    set new.total_price = new.pricePerUnit * new.quantity;
 	end$$
 delimiter ;
 
@@ -197,13 +197,13 @@ delimiter $$
 CREATE TRIGGER totalPO after update ON ProcurementOrderDetails for each row
     BEGIN
     declare sum double;
-    set sum = (select SUM(totalPrice) from ProcurementOrderDetails where pOrderID = new.pOrderID);
+    set sum = (select SUM(total_price) from ProcurementOrderDetails where pOrderID = new.pOrderID);
     update ProcurementOrder set total = sum where pOrderID = new.pOrderID;
 	end$$
 delimiter ;
 
 delimiter $$
-CREATE TRIGGER priceUpdatesPO before insert ON Statistics
+CREATE TRIGGER priceUpdatesStat before insert ON Statistics
  FOR EACH ROW 
 	BEGIN
     declare sumOrders double;
